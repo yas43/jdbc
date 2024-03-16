4e0662f1d57f87e3ee587d937debb9b69dbda5c7
yaser <y.r.k.r.2011@gmail.com>
2024-03-16 17:43:56 +0100

    editing

--- src/main/java/app.java
+++ src/main/java/app.java
@@ -2,18 +2,44 @@
 import java.sql.*;
 
 public class app {
-    public static void main(String[] args) throws Exception {
-        String sql = "SELECT postalcode FROM clientes WHERE country='india'";
-        String url = "jdbc:postgresql://localhost:5432/test";
-        String username = "yaser";
-        String password = "123456";
-        Connection connection = DriverManager.getConnection(url,username,password);
-        Statement statement = connection.createStatement();
-        ResultSet resultset = statement.executeQuery(sql);
-         resultset.next();
-        String postalcode = resultset.getString(1);
-        System.out.println(postalcode);
+    Connection connection;
+    public static void main(String[] args)  {
+
+    }
+
+   public void jdbcconection (){
+       String sql = "SELECT postalcode FROM clientes WHERE country='india'";
+       String url = "jdbc:postgresql://localhost:5432/test";
+       String username = "yaser";
+       String password = "123456";
+       try {
+           Connection connection = DriverManager.getConnection(url,username,password);
+           method();
+       } catch (SQLException e) {
+           e.printStackTrace();
+       }
+       finally {
+           try {
+               if(!connection.isClosed()){
+                   connection.close();
+               }
+           } catch (SQLException e) {
+               e.printStackTrace();
+           }
+       }
+
+   }
+
+    private void method() {
+    }
 
 
+    private void readdata(){
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery("SELECT * FROM student");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
